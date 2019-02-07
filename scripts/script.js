14044e7fb295bffe4503a9d886f6e997a6268aa9
Idity <idit.yeger@gmail.com>
2019-02-07 18:33:23 +0200

    weather images updated

--- scripts/script.js
+++ scripts/script.js
@@ -320,19 +320,19 @@ const knotsToKilometres = (knot) => {
 }
 
 const weatherImages = {
-  "clear-day": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Sun_icon.svg/252px-Sun_icon.svg.png",
-  "clear-night": "http://www.clker.com/cliparts/f/S/2/p/7/u/gold-matte-moon.svg",
-  "rain": "https://cdn3.iconfinder.com/data/icons/weather-16/256/Rainy_Day-512.png",
-  "snow": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Nuvola_weather_snow.svg/1000px-Nuvola_weather_snow.svg.png",
-  "sleet": "http://www.clker.com/cliparts/f/6/7/4/1206565674431593790Anonymous_simple_weather_symbols_10.svg.hi.png",
-  "wind": "http://www.haotu.net/up/4233/128/216-wind.png",
-  "fog": "http://www.iconninja.com/files/81/344/943/fog-cloud-hiding-the-sun-weather-interface-symbol-icon.svg",
-  "cloudy": "http://camera.thietbianninh.com/images/icon-2.png",
-  "partly-cloudy-day": "http://meteo.cw/images_www/weather_icons1/weather_icon_03.png",
-  "partly-cloudy-night": "http://icon-park.com/imagefiles/simple_weather_icons_cloudy_night.png",
-  "hail": "http://icons.iconarchive.com/icons/icons8/ios7/256/Weather-Hail-icon.png",
-  "thunderstorm": "http://findicons.com/files/icons/2613/android_weather_extended/480/thunderstorms.png",
-  "tornado": "http://hddfhm.com/images/clipart-of-a-tornado-11.png"
+  "clear-day": "../images/sunny.png",
+  "clear-night": "../images/clear-night.png",
+  "rain": "../images/light_rain.png",
+  "snow": "../images/snow4.png",
+  "sleet": "../images/sleet.png",
+  "wind": "../images/wind.png",
+  "fog": "../images/fog.svg",
+  "cloudy": "../images/cloudy5.png",
+  "partly-cloudy-day": "../images/cloudy2.png",
+  "partly-cloudy-night": "../images/cloudy2_night.png",
+  "hail": "../images/hail.png",
+  "thunderstorm": "../images/tstorm3.png",
+  "tornado": "../images/tornado2.png"
 }
 
 // Get the location and then continue to find the weather forecast accordingly
