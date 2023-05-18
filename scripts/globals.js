d7f7c600f9d3882f39eb44eec7b054ac28af0912
IditYegerSwimm <idit@swimm.io>
2023-05-18 15:11:30 +0300

    Update globals.js

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,7 +1,7 @@
 // Global variables initialization
 
 export const getImgSize = (alpha, container) => {
-  if (container.offsetWidth > container.offsetHeight) {
+  if (container.offsetWidth123 > container.offsetHeight) {
     return alpha*container.offsetHeight;
   } else {
     return alpha*container.offsetWidth;
